f0684e5d76ffce5bc4d7335f4b89aafbbf7ec791
LucaLF-dev <laficaraluca@gmail.com>
2023-11-02 16:06:58 +0100

    Add - generate and print into DOM email

--- js/app.js
+++ js/app.js
@@ -5,12 +5,25 @@ const { createApp } = Vue
 createApp({
 	data() {
 		return {
-			message: 'Hello',
+			arrayEmails: [],
+            email: '',
 		
 		}
 	},
 	methods: {	
-		
+		fetchEmail() {
+			axios
+				.get('https://flynn.boolean.careers/exercises/api/random/mail')
+				.then((risp) => {
+					console.log( risp.data.response)
+                    const emailGenerated = risp.data.response
+                    this.email = emailGenerated
+					
+				})
+		},
+	},
+    created() {
+		this.fetchEmail()
 	},
 	
 }).mount('#app')
