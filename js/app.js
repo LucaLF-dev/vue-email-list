e3d1c72b434bcb0b92af17e56783dd316b9f6e49
LucaLF-dev <laficaraluca@gmail.com>
2023-11-02 16:34:48 +0100

    Fixed: for in fetchEmail

--- js/app.js
+++ js/app.js
@@ -12,11 +12,11 @@ createApp({
 	},
 	methods: {	
 		fetchEmail() {
-            for (let i=1; i < 10; i++) {
+            for (let i = 1; i <= 10; i++) {
 			   axios
 				.get('https://flynn.boolean.careers/exercises/api/random/mail')
 				.then((risp) => {
-					console.log( risp.data.response)
+					// console.log( risp.data.response)
                     
                         const emailGenerated = risp.data.response
                         this.emailsList.push(emailGenerated)
