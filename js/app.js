e5553d0002265e0c0239505b009c05a8c12234fb
LucaLF-dev <laficaraluca@gmail.com>
2023-11-03 10:24:33 +0100

    modify: html style

--- js/app.js
+++ js/app.js
@@ -1,33 +1,27 @@
-const { createApp } = Vue
-
-
+const { createApp } = Vue;
 
 createApp({
 	data() {
 		return {
 			emailsList: [],
-            emailsNumber: 10,
-            
-		
-		}
+			emailsNumber: 10,
+		};
 	},
-	methods: {	
+	methods: {
 		fetchEmail() {
-            for (let i = 1; i <= this.emailsNumber; i++) {
-			   axios
-				.get('https://flynn.boolean.careers/exercises/api/random/mail')
-				.then((risp) => {
-					// console.log( risp.data.response)
-                    
-                        const emailGenerated = risp.data.response
-                        this.emailsList.push(emailGenerated)
-				});
-            }
+			for (let i = 1; i <= this.emailsNumber; i++) {
+				axios
+					.get("https://flynn.boolean.careers/exercises/api/random/mail")
+					.then((risp) => {
+						// console.log( risp.data.response)
+
+						const emailGenerated = risp.data.response;
+						this.emailsList.push(emailGenerated);
+					});
+			}
 		},
 	},
-    created() {
-
-		this.fetchEmail()
+	created() {
+		this.fetchEmail();
 	},
-	
-}).mount('#app')
+}).mount("#app");
